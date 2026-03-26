4bd131a3999432aae15d245f95cee065abef8003
Arivjoshi <arivjoshi@gmail.com>
2026-03-26 23:05:59 +0530

    Add User class with properties and methods

---
+++ User.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.Objects;
+
+public class User {
+    private String username;
+    private String password; // In production, hash this!
+    private double monthlyBudget;
+
+    public User(String username, String password, double monthlyBudget) {
+        this.username = username;
+        this.password = password;
+        this.monthlyBudget = monthlyBudget;
+    }
+
+    // Getters and Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public double getMonthlyBudget() { return monthlyBudget; }
+    public void setMonthlyBudget(double monthlyBudget) { this.monthlyBudget = monthlyBudget; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+}
